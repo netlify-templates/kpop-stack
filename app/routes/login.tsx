2a2da4f745c7b79bebfd1e7a523a2c0e46fd6e86
Prince Wilson <maxcell.wilson@gmail.com>
2022-03-18 15:26:42 -0400

    chore: Style the login page
    This uses the direct styles from the Remix stacks. Closes #20.

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -60,22 +60,73 @@ export default function Login() {
   }, [actionData]);
 
   return (
-    <div>
-      <Form method="post" noValidate>
-        <label htmlFor="email">Email Address</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" name="password" autoComplete="" />
-        <button type="submit">Log in</button>
-        <input type="hidden" name="redirectTo" value={redirectTo} />
-        <div>
-          <input id="remember" name="remember" type="checkbox" />
-          <label htmlFor="remember">Remember me</label>
-        </div>
-        <div>
-          Don't have an account? <Link to={{ pathname: "/join" }}>Sign up</Link>
-        </div>
-      </Form>
+    <div className="flex min-h-full flex-col justify-center">
+      <div className="mx-auto w-full max-w-md px-8">
+        <Form method="post" className="space-y-6" noValidate>
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-700"
+              htmlFor="email"
+            >
+              Email Address
+            </label>
+            <input
+              className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              autoComplete="email"
+              type="email"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-700"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              autoComplete=""
+              className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+            />
+          </div>
+          <button
+            className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+            type="submit"
+          >
+            Log in
+          </button>
+          <input type="hidden" name="redirectTo" value={redirectTo} />
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <input
+                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                id="remember"
+                name="remember"
+                type="checkbox"
+              />
+              <label
+                className="ml-2 block text-sm text-gray-900"
+                htmlFor="remember"
+              >
+                Remember me
+              </label>
+            </div>
+            <div className="text-center text-sm text-gray-500">
+              Don't have an account?{" "}
+              <Link
+                className="text-blue-500 underline"
+                to={{ pathname: "/join" }}
+              >
+                Sign up
+              </Link>
+            </div>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
