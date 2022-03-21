bc871acddd2190e8bee4597e1291121359c54a15
Prince Wilson <maxcell.wilson@gmail.com>
2022-03-21 08:44:43 -0400

    chore: Adds selected note view

--- app/models/note.server.ts
+++ app/models/note.server.ts
@@ -8,3 +8,23 @@ export async function getNoteListItems({ userId }: { userId: string }) {
 
   return data;
 }
+
+export async function getNote({ id, userId }) {
+  const { data, error } = await supabase
+    .from("notes")
+    .select("*")
+    .eq("profile_id", userId)
+    .eq("id", id)
+    .single();
+
+  if (!error) {
+    return {
+      userId: data.profile_id,
+      id: data.id,
+      title: data.title,
+      body: data.title,
+    };
+  }
+
+  return null;
+}
