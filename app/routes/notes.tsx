b87079343fbeeb0a80ec45d0d84811936e019825
Prince Wilson <maxcell.wilson@gmail.com>
2022-03-22 09:09:34 -0400

    chore (#27): Adds logout button (#30)

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,4 +1,5 @@
 import {
+  Form,
   json,
   Link,
   LoaderFunction,
@@ -66,6 +67,14 @@ function Header() {
         <Link to=".">Notes</Link>
       </h1>
       <p>{user.email}</p>
+      <Form action="/logout" method="post">
+        <button
+          type="submit"
+          className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+        >
+          Logout
+        </button>
+      </Form>
     </header>
   );
 }
