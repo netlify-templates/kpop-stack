2bce805a6e5ce4497d0625b45dbc3f3dd6c191e0
Prince Wilson <maxcell.wilson@gmail.com>
2022-06-10 12:15:19 -0400

    investigate: hardcoded url

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:8080',
     supportFile: false,
   },
 });
