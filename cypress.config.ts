77afcf841bd054458a40713836ec49644666814a
Prince Wilson <maxcell.wilson@gmail.com>
2022-06-14 15:43:59 -0400

    test: attemp 8080 with start

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      config.baseUrl = "http://localhost:3000"
+      config.baseUrl = "http://localhost:8080"
 
       return config
     }
