af9b79b239150dde0ecd62feba4c632f8de7f7fd
Prince Wilson <maxcell.wilson@gmail.com>
2022-06-14 17:04:19 -0400

    test: top-level baseUrl

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-
-      return config
-    }
+    baseUrl: "http://localhost:8888"
   },
 });
