07a680d72d5a5a55b583c53b346c7ba0a2474fdb
Prince Wilson <maxcell.wilson@gmail.com>
2022-06-14 14:09:06 -0400

    test: use updated cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      config.baseUrl = "http://localhost:3000"
+
+      return config
+    }
   },
 });
