3d695045ee637d31bcc8a4e7b5c175fa38b0124f
Prince Wilson <maxcell.wilson@gmail.com>
2022-06-14 16:39:17 -0400

    test: adds own baseUrl?

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      config.baseUrl = "http://localhost:8888"
 
       return config
     }
